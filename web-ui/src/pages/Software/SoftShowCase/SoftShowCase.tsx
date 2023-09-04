abba549a1f7d372366f35f8ae6e5f77758de2870
11smiley11 <bsmiley.lego@gmail.com>
2023-09-03 23:07:20 -0600

    Added more to software section and added button styles

--- web-ui/src/pages/Software/SoftShowCase/SoftShowCase.tsx
+++ web-ui/src/pages/Software/SoftShowCase/SoftShowCase.tsx
@@ -9,7 +9,7 @@ const SoftShowCase: React.FC<SoftShowCaseProps> = (props) => {
       id: 0,
       title: "Zombie Survival",
       content: (
-        <div>
+        <div className="software-card">
           <InfoCard
             subTitle="2D Shooter Game"
             description="A 2D shooter game where you have to survive waves of zombies. The game was created using Python and Pygame."
@@ -19,6 +19,15 @@ const SoftShowCase: React.FC<SoftShowCaseProps> = (props) => {
             }}
             through={[]}
           />
+
+          <a
+            href="https://github.com/b-smiley/ZombieSurvival"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link"
+          >
+            GitHub
+          </a>
         </div>
       ),
     },
@@ -26,7 +35,7 @@ const SoftShowCase: React.FC<SoftShowCaseProps> = (props) => {
       id: 1,
       title: "Mini Indoor Garden",
       content: (
-        <div>
+        <div className="software-card">
           <InfoCard
             subTitle="Automated Gardening System"
             description="A system that automatically waters plants and provides them with adequate light during the day. 
@@ -39,6 +48,65 @@ const SoftShowCase: React.FC<SoftShowCaseProps> = (props) => {
             }}
             through={[]}
           />
+          {/* <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link"
+          >
+            GitHub
+          </a> */}
+        </div>
+      ),
+    },
+    {
+      id: 2,
+      title: "Portfolio Website",
+      content: (
+        <div className="software-card">
+          <InfoCard
+            subTitle="Personal Website"
+            description="A website to showcase my projects and skills. The website was created on GitHub Pages using React and Typescript. Thank you GitHub for providing this service!"
+            skills={{
+              React: ["react", "React"],
+              Typescript: ["typescript", "Typescript"],
+            }}
+            through={[]}
+          />
+          {/* <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link"
+          >
+            GitHub
+          </a> */}
+        </div>
+      ),
+    },
+    {
+      id: 3,
+      title: "Schulich Ignite Blog",
+      content: (
+        <div className="software-card">
+          <InfoCard
+            subTitle="Student Mentoring and Blogging"
+            description="A University of Calgary club that helps students learn programming. I was a mentor for the club and wrote blog posts about a variety of software topics."
+            skills={{
+              Markdown: ["markdown", "Markdown"],
+              VBA: ["vba", "VBA"],
+              Python: ["python", "Python"],
+            }}
+            through={["Schulich Ignite"]}
+          />
+          <a
+            href="https://schulichignite.com/blog/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link"
+          >
+            Blog
+          </a>
         </div>
       ),
     },
