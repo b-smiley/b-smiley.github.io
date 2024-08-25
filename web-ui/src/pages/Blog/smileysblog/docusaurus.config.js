e91919c14dbeb55a07b28a6591e34998505b7b14
Brendan Smiley <bsmiley.lego@gmail.com>
2024-08-25 15:13:32 -0600

    Added SPI and DVS to software

--- web-ui/src/pages/Blog/smileysblog/docusaurus.config.js
+++ web-ui/src/pages/Blog/smileysblog/docusaurus.config.js
@@ -11,24 +11,14 @@ const config = {
   title: "Smiley's Blog",
   tagline: "It's not a bug, it's a feature! 🐞",
   favicon: "img/favicon.ico",
-
-  // Set the production url of your site here
   url: "https://brendansmiley.com",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/blog/",
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "b-smiley", // Usually your GitHub org/user name.
-  projectName: "Portfolio Website and Blog", // Usually your repo name.
+  organizationName: "b-smiley",
+  projectName: "Portfolio Website and Blog",
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
@@ -39,13 +29,6 @@ const config = {
       "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        // docs: {
-        //   sidebarPath: "./sidebars.js",
-        //   // Please change this to your repo.
-        //   // Remove this to remove the "edit this page" links.
-        //   editUrl:
-        //     "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-        // },
         blog: {
           routeBasePath: "posts",
           showReadingTime: true,
@@ -53,9 +36,8 @@ const config = {
             type: ["rss", "atom"],
             xslt: true,
           },
-          // Please change this to your repo.
-          editUrl: "https://github.com/b-smiley/b-smiley.github.io",
-          // Useful options to enforce blogging best practices
+          editUrl:
+            "https://github.com/b-smiley/b-smiley.github.io/blob/main/web-ui/src/pages/Blog/smileysblog/",
           onInlineTags: "warn",
           onInlineAuthors: "warn",
           onUntruncatedBlogPosts: "warn",
