e18038c4dce55cee21ef8c4d6d3f8150e304f168
Brendan Smiley <bsmiley.lego@gmail.com>
2024-08-24 23:57:54 -0600

    Added main page of blog
    Could use more spark but good enough for now.

--- web-ui/src/pages/Blog/smileysblog/src/pages/index.js
+++ web-ui/src/pages/Blog/smileysblog/src/pages/index.js
@@ -1,26 +1,23 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
 
-import Heading from '@theme/Heading';
-import styles from './index.module.css';
+import Heading from "@theme/Heading";
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+          <Link className="button button--secondary button--lg" to="/posts">
+            Blog Posts
           </Link>
         </div>
       </div>
@@ -29,14 +26,25 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="A blog about software, mechanical engineering, and other topics."
+    >
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <div
+          className="welcome"
+          style={{ textAlign: "center", padding: "50px" }}
+        >
+          <h1>Welcome to Smiley's Blog!</h1>
+          <p>
+            This blog is a place for me to share my thoughts on software,
+            mechanical engineering, and other topics. I hope you find it
+            interesting and informative!
+          </p>
+        </div>
       </main>
     </Layout>
   );
